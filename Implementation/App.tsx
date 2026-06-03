0077cc16b04277327c5cd25e5478925575e02f8b
bhotee <rojan@dsu.ac.kr>
2026-06-03 19:45:28 +0900

    FIXING

--- Implementation/App.tsx
+++ Implementation/App.tsx
@@ -25,7 +25,6 @@ import AddExpenseScreen from './src/screens/expenses/AddExpenseScreen';
 import ExpenseDetailScreen from './src/screens/expenses/ExpenseDetailScreen';
 import EditExpenseScreen from './src/screens/expenses/EditExpenseScreen';
 import ScanReceiptScreen from './src/screens/expenses/ScanReceiptScreen';
-import SelectGroupScreen from './src/screens/expenses/SelectGroupScreen';
 
 // Settlement
 import SettleUpScreen from './src/screens/expenses/SettleUpScreen';
@@ -60,7 +59,6 @@ type Screen =
   | 'edit-expense'
   | 'settle-up'
   | 'scan-receipt'
-  | 'select-group'
   | 'notification-settings'
   | 'edit-profile'
   | 'analytics'
@@ -163,14 +161,9 @@ function AppNavigator() {
     return (
       <SelectGroupScreen
         onBack={() => setScreen('home')}
-<<<<<<< HEAD
         onSelect={(info) => {
           setGroupContext(info);
           setAddExpenseSource('select-group');
-=======
-        onSelectGroup={(info) => {
-          setGroupContext(info);
->>>>>>> 36ac9c4c331d08e03da1faf6472748897ef56968
           setScreen('add-expense');
         }}
       />
@@ -416,14 +409,10 @@ function AppNavigator() {
           setActiveGroupId(id);
           setScreen('group');
         }}
-<<<<<<< HEAD
-        onAddExpense={() => setScreen('select-group')}
-=======
         onAddExpense={() => {
           setGroupContext(null);
           setScreen('select-group');
         }}
->>>>>>> 36ac9c4c331d08e03da1faf6472748897ef56968
         onScanReceipt={() => setScreen('scan-receipt')}
         onCreateGroup={() => setScreen('create-group')}
         onOpenNotifications={() => setActiveTab('activity')}
